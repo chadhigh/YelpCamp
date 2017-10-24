85ca1d05a9e580a1824ed3361be43ab9b728e02e
Chad High <cdhqwebdev@gmail.com>
2017-10-24 20:49:25 +0000

    final styling commit and deploy

--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,21 @@ router.get("/", function(req, res){
 });
 
 
-
-
-
 //================
 //AUTH ROUTES
 //================
 
 //show register form
 router.get("/register", function(req, res) {
-   res.render("register"); 
+   res.render("register", {page: 'register'}); 
 });
 //handle sign up logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            return res.render("register", {"error": err.message});
+            console.log(err);
+            return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
             req.flash("success", "welcome to YelpCamp " + user.username);
@@ -36,7 +34,7 @@ router.post("/register", function(req, res){
 
 //show login form
 router.get("/login", function(req, res) {
-   res.render("login"); 
+   res.render("login", {page: 'login'}); 
 });
 //handling login logic
 router.post("/login", passport.authenticate('local', 
